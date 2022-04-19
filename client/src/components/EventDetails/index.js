1812da5661aa569216f87fa05a86773d16887c35
adairconlin <adair.conlin@gmail.com>
2022-04-19 15:44:21 -0400

    Add save prompt message to saving event update

--- client/src/components/EventDetails/index.js
+++ client/src/components/EventDetails/index.js
@@ -27,8 +27,32 @@ const EventDetails = ({ eventId, onClose, dateInfo }) => {
         });
     }
 
+    const promptSave = e => {
+        const container = document.querySelector(".eventDetailContainer");
+        const warningDiv = document.createElement("div");
+        const warningMessage = document.createElement("p");
+        warningMessage.textContent = "Are you sure you want to save?";
+        const saveBtn = document.createElement("button");
+        const cancelBtn = document.createElement("button");
+        saveBtn.textContent = "Save";
+        saveBtn.addEventListener("click", updateEvent);
+        cancelBtn.textContent = "Cancel";
+        cancelBtn.addEventListener("click", cancelSave);
+
+        container.appendChild(warningDiv)
+        warningDiv.appendChild(warningMessage);
+        warningDiv.appendChild(saveBtn);
+        warningDiv.appendChild(cancelBtn);
+    }
+
+    const cancelSave = () => {
+        const container = document.querySelector(".eventDetailContainer");
+        container.removeChild(container.lastChild);
+    }
+
     const [editEvent, { error }] = useMutation(EDIT_EVENT);
     const updateEvent = async (e) => {
+        console.log("test updateEvent");
         e.preventDefault();
         try {
             const { data } = await editEvent({
@@ -55,9 +79,15 @@ const EventDetails = ({ eventId, onClose, dateInfo }) => {
            <div className="eventDetailContainer">
                <button onClick={onClose}>X</button>
                <p>{todaysDate}</p>
-               <textarea onChange={handleChange} onBlur={updateEvent} defaultValue={currentEvent?.title} name="title" />
-               <textarea onChange={handleChange} onBlur={updateEvent} defaultValue={currentEvent?.description} name="description" />
+               <textarea onChange={handleChange} defaultValue={currentEvent?.title} name="title" />
+               <textarea onChange={handleChange} defaultValue={currentEvent?.description} name="description" />
+               {currentEvent?.category ?
+                    <textarea onChange={handleChange} defaultValue={currentEvent?.category} name="category" />
+                :
+                    <button>Add Category</button>
+               }
                {error && <p>There was an error with your request.</p>}
+               <button onClick={promptSave}>Save Changes</button>
            </div>
         </div>
     )
